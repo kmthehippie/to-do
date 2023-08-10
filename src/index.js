580905616287ef19fd309096e1a6852903a9f84c
kmthehippie <66264446+kmthehippie@users.noreply.github.com>
2023-08-10 20:36:26 +0800

    Reorganize data for webpack

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-function component(){
-    const element = document.createElement("div");
-    element.innerHTML = ("Hello");
-    return element;
-}
+import { navAddTask } from "./javascript/general.js";
+import { taskStorage } from "./javascript/storage_tasks.js";
+import './styles.css';
 
-document.body.appendChild(component());
+document.addEventListener('DOMContentLoaded', ()=>{
+    taskStorage.storageRetrieved();
+    navAddTask()
+})
